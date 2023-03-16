5c93d93e1c88a01b05ebc76d24db60fabd37819f
VladislavK <vladkouznecov@gmail.com>
2023-03-16 23:57:24 +0200

    Add file rectangular

--- Ideal_weight.rb
+++ Ideal_weight.rb
@@ -1,12 +1,13 @@
+#Идеальный вес
 puts "Расчитаем ваш идеальный вес"
 puts "Введите ваше имя"
 name = STDIN.gets.chomp
 puts "Введите ваш рост в сантиметрах"
-height = STDIN.gets.to_i
+height = STDIN.gets.to_f
 ideal = (height - 110) * 1.15
 
 if ideal < 0
     puts "Ваш вес уже оптимальный"
 else
-    puts "#{name}, ваш идеальный вес #{ideal.round(0)} килограмм"
+    puts "#{name}, ваш идеальный вес #{ideal.round(1)} килограмм"
 end
