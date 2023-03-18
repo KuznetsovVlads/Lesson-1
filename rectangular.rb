a5e36477794d6f3fe458d9a2ffe171ea3fd4fa17
VladislavK <vladkouznecov@gmail.com>
2023-03-18 23:15:01 +0200

    rewrittten rectangular.rb by the method 'guard clause'

--- rectangular.rb
+++ rectangular.rb
@@ -5,38 +5,14 @@ puts "Введите размер второй стороны треугольн
 b = gets.to_f
 puts "Введите размер третьей стороны треугольника"
 c = gets.to_f
-if a + b > c && a + c > b && b + c > a
     
-    right = a == b && b == c
-    
+abort("Треугольник не существует") if a + b < c || a + c < b || b + c < a
     max = [a, b, c].sort
 
-    rectangle = max[2] ** 2 == max[0] ** 2 + max[1] ** 2
-    
-    isosceles = a == b && a != c || a == c && a != b || b == c && b != a
-    
-    both = false
+puts "Треугольник равносторонний" if a == b && b == c
 
-    if rectangle && isosceles
-        if a == b || b == c || c == a
-            both = true
-        end
-    end
+puts "Треугольник и равнобедренный и прямоугольный" if max[2] ** 2 == max[0] ** 2 + max[1] ** 2 && (a == b && a != c || a == c && a != b || b == c && b != a)
 
-    case 
-        when both
-            puts "Треугольник и равнобедренный и прямоугольный"
-        when right 
-            puts "Треугольник равносторонний"
-        when isosceles
-            puts "Треугольник равнобедренный"
-        when rectangle
-            puts "Треугольник прямоугольный"
-        else
-            puts "Это обычный треугольник"
-    end
-    
-else
-    puts "Треугольник не существует"
+puts "Треугольник равнобедренный" if a == b && a != c || a == c && a != b || b == c && b != a
 
-end
+puts "Треугольник прямоугольный" if max[2] ** 2 == max[0] ** 2 + max[1] ** 2
